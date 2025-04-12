c36c46af7366e73e00e04193fea9aa78226e3c02
Afonughe Anthony <57123228+ToniGift@users.noreply.github.com>
2025-04-12 02:51:22 +0000

    build-octofit-app

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-kpr6w+70dimz)jqrd@_3^v0_!)fo0m_2#owx5oyzyn^e=+6sun
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zany-space-orbit-655g4pwggqgh5gpr.github.dev', 'localhost']
 
 
 # Application definition
